b5f7a15497ff500b0b42420d5ad4cc7fb6c9ea03
Conrado Pinheiro <conradopsa@gmail.com>
2021-01-24 02:39:34 -0300

    removing trash

--- src/main/java/io/conrado/api/starwars/controllers/PlanetaController.java
+++ src/main/java/io/conrado/api/starwars/controllers/PlanetaController.java
@@ -11,8 +11,6 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.mongodb.MongoWriteException;
-
 import org.bson.conversions.Bson;
 
 import io.conrado.api.starwars.models.Planeta;
